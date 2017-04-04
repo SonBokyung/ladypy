79bc447078e5e1f45c69a0f7da629378d5e4ba6f
PluVian <urisa12@gmail.com>
2017-04-04 14:55:55 +0900

    Seperate parameter k and K for role model learning & random learning.

--- elg/learn.py
+++ elg/learn.py
@@ -4,7 +4,7 @@ from .sample import sample_from_P
 
 
 def learn(Bs, props, ks, eps, rho):
-    k_prt, k_rol, k_rnd = ks
+    k_prt, k_rol, K_rol, k_rnd, K_rnd = ks
     n, m = Bs[0].P.shape
 
     A = np.zeros(Bs[0].P.shape)
@@ -15,14 +15,14 @@ def learn(Bs, props, ks, eps, rho):
         idx = np.delete(idx, prt)
         A += sample_from_P(Bs[prt].P, k_prt, rho)
 
-    if k_rol > 0:
-        mdls = np.random.choice(idx, k_rol, p=props, replace=False)
+    if k_rol > 0 and K_rol > 0:
+        mdls = np.random.choice(idx, K_rol, p=np.array(props)[idx], replace=False)
         idx = np.setdiff1d(idx, mdls)
         for mdl in mdls:
             A += sample_from_P(Bs[mdl].P, k_rol, rho)
 
-    if k_rnd > 0:
-        rnds = np.random.choice(idx, k_rnd, replace=False)
+    if k_rnd > 0 and K_rnd > 0:
+        rnds = np.random.choice(idx, K_rnd, replace=False)
         idx = np.setdiff1d(idx, rnds)
         for rnd in rnds:
             A += sample_from_P(Bs[rnd].P, k_rnd, rho)
