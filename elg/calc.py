4ad8f775024ffcada922b4b9e22a39c1710af2bb
PluVian <urisa12@gmail.com>
2017-04-04 14:03:09 +0900

    Changed arguments name for Agents

--- elg/calc.py
+++ elg/calc.py
@@ -3,27 +3,31 @@ from .agent import Agent
 
 
 def payoff(A, B):
-    return 0.5 * (np.einsum('ij,ji->', A.P, B.Q) +
-                  np.einsum('ij,ji->', B.P, A.Q))
+    return 0.5 * (
+        np.einsum('ij,ji->', A.P, B.Q) + np.einsum('ij,ji->', B.P, A.Q))
 
 
-def total_payoff(A, Bs):
-    return sum([payoff(A, B) for B in Bs])
+def total_payoff(A, Agents):
+    return sum([payoff(A, B) for B in Agents])
 
 
-def calc_payoff(As):
-    return [payoff(a, a) for a in As]
+def calc_payoff(Agents):
+    return [payoff(A, A) for A in Agents]
 
 
-def calc_payoff_total(As):
-    return [total_payoff(a, As[:i] + As[(i + 1):]) for i, a in enumerate(As)]
+def calc_payoff_total(Agents):
+    return [
+        total_payoff(a, Agents[:i] + Agents[(i + 1):])
+        for i, a in enumerate(Agents)
+    ]
 
 
-def calc_payoff_avg(As):
-    l = len(As) - 1
-    return [val / l for val in calc_payoff_total(As)]
+def calc_payoff_avg(Agents):
+    l = len(Agents) - 1
+    return [val / l for val in calc_payoff_total(Agents)]
 
 
-def calc_probs(As):
-    tp = calc_payoff_total(As)
+def calc_probs(Agents):
+    tp = calc_payoff_total(Agents)
     return tp / sum(tp)
+
