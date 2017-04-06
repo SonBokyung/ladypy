366e69c88bda31207d33c3d7246f69ea4044115c
PluVian <urisa12@gmail.com>
2017-04-06 11:27:02 +0900

    Changed for code readability.

--- elg/sample.py
+++ elg/sample.py
@@ -5,9 +5,10 @@ from .agent import Agent
 def sample_from_P_without_rho(P, k):
     n, m = P.shape
 
+    rows = [np.random.choice(m, k, p=P[i,:]) for i in range(v)]
+
     return np.concatenate([
-        np.bincount(np.random.choice(m, k, p=P[i, :]), minlength=m)
-        for i in range(n)
+        np.bincount(row, minlength=m) for i in range(n)
     ]).reshape(n, m).astype(np.float64)
 
 
@@ -20,8 +21,11 @@ def sample_from_P(P, k, rho):
     rows = [
         np.where(
             np.random.rand(m) > rho,
-            np.random.choice(m, m, p=P[i, :]), np.random.choice(m, m))
+            np.random.choice(m, m, p=P[i,:]),
+            np.random.choice(m, m))
         for i in range(n)
     ]
-    return np.concatenate([np.bincount(row, minlength=m)
-                           for row in rows]).reshape(n, m).astype(np.float64)
+
+    return np.concatenate([
+        np.bincount(row, minlength=m) for row in rows
+    ]).reshape(n, m).astype(np.float64)
