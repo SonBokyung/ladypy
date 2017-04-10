2f8422a3554b8ea53b3be154c96b9dc882de0e7e
PluVian <urisa12@gmail.com>
2017-04-10 20:44:24 +0900

    Removed codes of deprecated version.

--- ex_elg/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import division, absolute_import, print_function
-
-from .agent import derive_P, derive_Q, Agent
-from .learn import learn
-from .calc import payoff, calc_payoff, calc_payoff_avg, calc_probs
-from .sample import sample_from_P
-from .simulate import simulate
-
-from . import data
--- ex_elg/data/env.py
+++
@@ -1,4 +0,0 @@
-_ks = [1, 4, 7, 10]
-Ksets = [[(i, 0, 0, 0, 0) for i in _ks],
-         [(0, i, 1, 0, 0) for i in _ks],
-         [(0, 0, 0, i, 1) for i in _ks]]
