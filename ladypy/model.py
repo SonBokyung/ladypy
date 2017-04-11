904ab07eacb022363189661a80f4ec40add25608
PluVian <urisa12@gmail.com>
2017-04-11 14:24:15 +0900

    Changed ELG model structure.

--- ladypy/model.py
+++ ladypy/model.py
@@ -13,90 +13,74 @@ def initialize_A(n_pep, n_obj, n_sig):
     return np.random.random(size=(n_pep, n_obj, n_sig))
 
 
-class BaseSimulation:
-    """ Base class for simulation.
-    """
-    N_REP = 10
-    N_GEN = 100
-    N_POP = 100
-    N_OBJ = 5
-    N_SIG = 5
-
+class ELG:
     def __init__(self, **kargs):
-        self.N_REP = kargs.get('rep', 10)
+        self.set_config(**kargs)
+
+    def set_config(self, **kargs):
         self.N_GEN = kargs.get('gen', 100)
         self.N_POP = kargs.get('pop', 100)
         self.N_OBJ = kargs.get('obj', 5)
         self.N_SIG = kargs.get('sig', 5)
-
-
-class ELG(BaseSimulation):
-    def __init__(self, **kargs):
-        super(ELG, self).__init__(**kargs)
-
-    def _generate_param_K(self, **kargs):
-        return {
-            'parent': kargs.get('parent', 0),
-            'rolemodel': kargs.get('rolemodel', 0),
-            'random': kargs.get('random', 0)
+        self.K = {
+            'k_par': kargs.get('k_par', 0),
+            'K_rol': kargs.get('K_rol', 0),
+            'k_rol': kargs.get('k_rol', 1),
+            'K_rnd': kargs.get('K_rnd', 0),
+            'k_rnd': kargs.get('k_rnd', 1)
         }
+        self.eps = kargs.get('eps', 1e-3)
+        self.rho = kargs.get('rho', None)
 
-    def _evolute(self, P, pr, K, eps, rho):
-        A = eps * initialize_A(self.N_POP, self.N_OBJ, self.N_SIG)
+    def _evolute(self, P, pr):
+        A = self.eps * initialize_A(self.N_POP, self.N_OBJ, self.N_SIG)
 
         for i in range(self.N_POP):
             idx = np.arange(self.N_POP)
 
-            if K['parent'] > 0:
+            if self.K['k_par'] > 0:
                 i_prt = np.random.choice(idx, p=pr)
                 idx = np.delete(idx, i_prt)
-                A[i] += sample_response(P[i_prt], K['parent'], rho)
+                A[i] += sample_response(P[i_prt], self.K['k_par'], self.rho)
 
-            if K['rolemodel'] > 0:
+            if self.K['K_rol'] > 0:
                 pr_mdl = pr[idx] / pr[idx].sum()
                 i_mdls = np.random.choice(
-                    idx, K['rolemodel'], p=pr_mdl, replace=False)
+                    idx, self.K['K_rol'], p=pr_mdl, replace=False)
                 idx = np.setdiff1d(idx, i_mdls)
                 for i_mdl in i_mdls:
-                    A[i] += sample_response(P[i_mdl], 1, rho)
+                    A[i] += sample_response(P[i_mdl], self.K['k_rol'],
+                                            self.rho)
 
-            if K['random'] > 0:
-                i_rnds = np.random.choice(idx, K['random'], replace=False)
+            if self.K['K_rnd'] > 0:
+                i_rnds = np.random.choice(idx, self.K['K_rnd'], replace=False)
                 for i_rnd in i_rnds:
-                    A[i] += sample_response(P[i_rnd], 1, rho)
+                    A[i] += sample_response(P[i_rnd], self.K['k_rnd'],
+                                            self.rho)
 
         return A
 
-    def run(self, **kargs):
+    def run(self):
         """ Run the simulation with the given parameters K, eps, rho.
         """
-        tracks = []
-        K = self._generate_param_K(**kargs)
-        eps = kargs.get('eps', 1e-3)
-        rho = kargs.get('rho', None)
+        A = initialize_A(self.N_POP, self.N_OBJ, self.N_SIG)
+        P = derive_P_from(A)
+        Q = derive_Q_from(A)
 
-        for trial in trange(1, self.N_REP + 1, desc='\u2514\u2500Trial'):
-            track = np.zeros(self.N_GEN + 1)
+        po = payoff_PQ(P, Q)
+        prob = po / po.sum()
 
-            A = initialize_A(self.N_POP, self.N_OBJ, self.N_SIG)
+        track = np.zeros(self.N_GEN + 1)
+        track[0] = po.sum() / self.N_POP
+
+        for epoch in trange(1, self.N_GEN + 1, desc='Epoch'):
+            A = self._evolute(P, prob)
             P = derive_P_from(A)
             Q = derive_Q_from(A)
 
             po = payoff_PQ(P, Q)
             prob = po / po.sum()
 
-            track[0] = po.sum() / self.N_POP
+            track[epoch] = po.sum() / self.N_POP
 
-            for epoch in trange(1, self.N_GEN + 1, desc='  \u2514\u2500Epoch'):
-                A = self._evolute(P, prob, K, eps, rho)
-                P = derive_P_from(A)
-                Q = derive_Q_from(A)
-
-                po = payoff_PQ(P, Q)
-                prob = po / po.sum()
-
-                track[epoch] = po.sum() / self.N_POP
-
-            tracks.append(track)
-
-        return tracks
+        return A, track
