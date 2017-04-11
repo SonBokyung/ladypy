2592aaa5b1ff8a5420cc44e15e41e3557bc3317b
PluVian <urisa12@gmail.com>
2017-04-11 14:40:31 +0900

    Changed test codes following changes of model structure.

--- tools/test_elg_models.py
+++ tools/test_elg_models.py
@@ -1,46 +1,56 @@
 import sys
 import os
+
 import numpy as np
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
+
 from tqdm import tqdm, trange
 from ladypy.model import ELG
 
-N_REP = 20
-N_GEN = 100
-N_POP = 100
-N_OBJ = 5
-N_SIG = 5
 
+def draw_simulated_graph(ax, conf):
+    sim = ELG(**conf)
 
-def draw_simulated_graph(ax, ks, rho):
-    sim = ELG(rep=N_REP, gen=N_GEN, pop=N_POP, obj=N_OBJ, sig=N_SIG)
-    results = sim.run(parent=ks[0], rolemodel=ks[1], random=ks[2], rho=rho)
-
-    for result in results:
+    for rep in trange(conf['rep'], desc='Trial'):
+        A, result = sim.run()
         ax.plot(result, ls='-', alpha=.5)
 
-    for h in range(1, min(N_OBJ, N_SIG) + 1):
+    for h in range(1, min(conf['obj'], conf['sig']) + 1):
         ax.axhline(h, ls='dotted', c='k', alpha=.25)
 
-    title = '{rep} Simulations with {pop} Agents'.format(rep=N_REP, pop=N_POP)
-    option = '{}x{} matrix P, K=({}, {}, {}), rho={}'.format(
-        N_OBJ, N_SIG, ks[0], ks[1], ks[2], rho)
+    title = \
+        ('{rep} Simulations with {pop} Agents\n'
+         '{obj}x{sig} matrix P, '
+         'K=({k_par}, {K_rol}, {k_rol}, {K_rnd}, {k_rnd}), '
+         'rho={rho}').format(**conf)
 
-    ax.set_title(title + '\n' + option, fontweight='bold')
+    ax.set_title(title, fontweight='bold')
     ax.set_xlabel('Generation')
     ax.set_ylabel('Payoff')
-    ax.axis([0, N_GEN, 0, min(N_OBJ, N_SIG) + 1])
+    ax.axis([0, conf['gen'], 0, min(conf['obj'], conf['sig']) + 1])
+
+    return A
 
 
 if __name__ == '__main__':
     fig, axes = plt.subplots(nrows=2, ncols=2)
     fig.set_size_inches(12, 10)
 
-    for i in trange(len(axes.flat), desc='Chart'):
-        draw_simulated_graph(axes.flat[i], [3 * i + 1, 3 * i + 1, 0, 0], 0)
+    for i, ax in enumerate(axes.flat):
+        draw_simulated_graph(ax, {
+            'rep': 20,
+            'gen': 1000,
+            'pop': 100,
+            'obj': 5,
+            'sig': 5,
+            'k_par': 0,
+            'K_rol': 3 * i + 1,
+            'k_rol': 1,
+            'K_rnd': 0,
+            'k_rnd': 0,
+        })
 
     plt.tight_layout()
-
     plt.savefig('output_test3.png')
