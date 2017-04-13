defba68aae7207bdb37eb2db9cb74663116f3419
PluVian <urisa12@gmail.com>
2017-04-13 11:19:48 +0900

    Changed description.

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup
 
-
-setup(name='ladypy',
-      version='alpha',
-      description='Python library for simulating models of Language Dynamics',
-      long_description='',
-      classifiers=[],
-      keywords=[],
-      url='https://github.com/PluVian/ladypy',
-      author='PluVian',
-      author_email='urisa12@gmail.com',
-      license='MIT',
-      packages=['ladypy'],
-      install_requires=[
-          'numpy',
-      ],
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    name='ladypy',
+    version='alpha',
+    description='''Library for simulating models of Language Dynamics using
+    NumPy.''',
+    long_description='',
+    classifiers=[],
+    keywords=[],
+    url='https://github.com/PluVian/ladypy',
+    author='PluVian',
+    author_email='urisa12@gmail.com',
+    license='MIT',
+    packages=['ladypy'],
+    install_requires=[
+        'numpy',
+    ],
+    include_package_data=True,
+    zip_safe=False)
