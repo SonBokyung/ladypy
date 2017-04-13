7cf7072a2d1a4f64c11c187d91b909722bc0d903
PluVian <urisa12@gmail.com>
2017-04-13 09:51:55 +0900

    Changed the interface of ELG class.

--- ladypy/model.py
+++ ladypy/model.py
@@ -1,25 +1,17 @@
 from __future__ import division, absolute_import
 import numpy as np
-from tqdm import tqdm, trange
 from .calc import derive_P_from, derive_Q_from, payoff_PQ
 from .sample import sample_response
 
 __all__ = ['ELG']
 
 
-def initialize_A(n_pep, n_obj, n_sig):
-    """ Generate initial association matrices for simulation.
-        Works as a wrapper of generating random matrix of `numpy.ndarray`.
-    """
-    return np.random.random(size=(n_pep, n_obj, n_sig))
-
-
 class ELG:
     def __init__(self, **kargs):
         self.set_config(**kargs)
+        self.initialize()
 
     def set_config(self, **kargs):
-        self.N_GEN = kargs.get('gen', 100)
         self.N_POP = kargs.get('pop', 100)
         self.N_OBJ = kargs.get('obj', 5)
         self.N_SIG = kargs.get('sig', 5)
@@ -30,11 +22,27 @@ class ELG:
             'K_rnd': kargs.get('K_rnd', 0),
             'k_rnd': kargs.get('k_rnd', 1)
         }
-        self.eps = kargs.get('eps', 1e-3)
-        self.rho = kargs.get('rho', None)
+        self.EPS = kargs.get('eps', 1e-3)
+        self.RHO = kargs.get('rho', None)
 
-    def _evolute(self, P, pr):
-        A = self.eps * initialize_A(self.N_POP, self.N_OBJ, self.N_SIG)
+    def initialize(self):
+        """ Initialize agents in the model with random values.
+        """
+        assert isinstance(self.N_POP, int)
+        assert isinstance(self.N_OBJ, int)
+        assert isinstance(self.N_SIG, int)
+
+        self.A = np.random.random((self.N_POP, self.N_OBJ, self.N_SIG))
+        self.P = derive_P_from(self.A)
+        self.Q = derive_Q_from(self.A)
+        self.payoff = payoff_PQ(self.P, self.Q)
+
+    def evolve(self):
+        """ Generate new child epoch with the current epoch.
+        """
+        pr = self.payoff / self.payoff.sum()
+
+        An = self.EPS * np.random.random((self.N_POP, self.N_OBJ, self.N_SIG))
 
         for i in range(self.N_POP):
             idx = np.arange(self.N_POP)
@@ -42,7 +50,8 @@ class ELG:
             if self.K['k_par'] > 0:
                 i_prt = np.random.choice(idx, p=pr)
                 idx = np.delete(idx, i_prt)
-                A[i] += sample_response(P[i_prt], self.K['k_par'], self.rho)
+                An[i] += sample_response(self.P[i_prt], self.K['k_par'],
+                                         self.RHO)
 
             if self.K['K_rol'] > 0:
                 pr_mdl = pr[idx] / pr[idx].sum()
@@ -50,38 +59,19 @@ class ELG:
                     idx, self.K['K_rol'], p=pr_mdl, replace=False)
                 idx = np.setdiff1d(idx, i_mdls)
                 for i_mdl in i_mdls:
-                    A[i] += sample_response(P[i_mdl], self.K['k_rol'],
-                                            self.rho)
+                    An[i] += sample_response(self.P[i_mdl], self.K['k_rol'],
+                                             self.RHO)
 
             if self.K['K_rnd'] > 0:
                 i_rnds = np.random.choice(idx, self.K['K_rnd'], replace=False)
                 for i_rnd in i_rnds:
-                    A[i] += sample_response(P[i_rnd], self.K['k_rnd'],
-                                            self.rho)
+                    An[i] += sample_response(self.P[i_rnd], self.K['k_rnd'],
+                                             self.RHO)
 
-        return A
+        self.A = An
+        self.P = derive_P_from(An)
+        self.Q = derive_Q_from(An)
+        self.payoff = payoff_PQ(self.P, self.Q)
 
-    def run(self):
-        """ Run the simulation with the given parameters K, eps, rho.
-        """
-        A = initialize_A(self.N_POP, self.N_OBJ, self.N_SIG)
-        P = derive_P_from(A)
-        Q = derive_Q_from(A)
-
-        po = payoff_PQ(P, Q)
-        prob = po / po.sum()
-
-        track = np.zeros(self.N_GEN + 1)
-        track[0] = po.sum() / self.N_POP
-
-        for epoch in trange(1, self.N_GEN + 1, desc='Epoch'):
-            A = self._evolute(P, prob)
-            P = derive_P_from(A)
-            Q = derive_Q_from(A)
-
-            po = payoff_PQ(P, Q)
-            prob = po / po.sum()
-
-            track[epoch] = po.sum() / self.N_POP
-
-        return A, track
+    def fitness(self):
+        return self.payoff.sum() / self.N_POP
--- tools/test_elg_models.py
+++ tools/test_elg_models.py
@@ -1,4 +1,6 @@
 from __future__ import print_function, division, unicode_literals
+from builtins import input
+
 import numpy as np
 import matplotlib as mpl
 mpl.use('Agg')
@@ -8,10 +10,8 @@ from tqdm import tqdm, trange
 from ladypy.model import ELG
 
 
-def generate_conf(**kargs):
+def mk_conf(**kargs):
     return {
-        'rep': kargs.get('rep', 20),
-        'gen': kargs.get('gen', 1000),
         'pop': kargs.get('pop', 100),
         'obj': kargs.get('obj', 5),
         'sig': kargs.get('sig', 5),
@@ -25,12 +25,16 @@ def generate_conf(**kargs):
     }
 
 
-def draw_simulated_graph(ax, conf):
-    sim = ELG(**conf)
+def draw_simulated_graph(ax, rep, gen, conf):
+    mdl = ELG(**conf)
 
-    for rep in trange(conf['rep'], desc='Trial'):
-        A, result = sim.run()
-        ax.plot(result, ls='-', alpha=.5)
+    for _ in trange(rep, desc='Trial'):
+        mdl.initialize()
+        dat = [mdl.fitness()]
+        for _ in trange(gen, desc='Epoch'):
+            mdl.evolve()
+            dat.append(mdl.fitness())
+        ax.plot(dat, ls='-', alpha=.5)
 
     for h in range(1, min(conf['obj'], conf['sig']) + 1):
         ax.axhline(h, ls='dotted', c='k', alpha=.25)
@@ -39,40 +43,49 @@ def draw_simulated_graph(ax, conf):
         ('{rep} Simulations with {pop} Agents\n'
          '{obj}x{sig} matrix P, '
          'K=({k_par}, {K_rol}, {k_rol}, {K_rnd}, {k_rnd}), '
-         'rho={rho}').format(**conf)
+         'rho={rho}').format(rep=rep, **conf)
 
     ax.set_title(title, fontweight='bold')
     ax.set_xlabel('Generation')
     ax.set_ylabel('Payoff')
-    ax.axis([0, conf['gen'], 0, min(conf['obj'], conf['sig']) + 1])
-
-    return A
+    ax.axis([0, gen, 0, min(conf['obj'], conf['sig']) + 1])
 
 
 if __name__ == '__main__':
-    fig, axes = plt.subplots(nrows=2, ncols=2)
-    fig.set_size_inches(12, 10)
+    tqdm.write('Run the simulation about parental learning [y/N]: ', end='')
+    ans = input().lower()
+    if ans == 'y' or ans == 'yes':
+        fig, axes = plt.subplots(nrows=2, ncols=2)
+        fig.set_size_inches(12, 10)
 
-    for i, ax in enumerate(axes.flat):
-        draw_simulated_graph(ax, generate_conf(gen=100, k_par=3 * i + 1))
+        for i, ax in enumerate(axes.flat):
+            draw_simulated_graph(ax, 20, 100, mk_conf(k_par=3 * i + 1))
 
-    plt.tight_layout()
-    plt.savefig('output_1.png')
+        plt.tight_layout()
+        plt.savefig('output_1.png')
 
-    fig, axes = plt.subplots(nrows=2, ncols=2)
-    fig.set_size_inches(12, 10)
+    tqdm.write('Run the simulation about role model learning [y/N]: ', end='')
+    ans = input().lower()
+    if ans == 'y' or ans == 'yes':
+        fig, axes = plt.subplots(nrows=2, ncols=2)
+        fig.set_size_inches(12, 10)
 
-    for i, ax in enumerate(axes.flat):
-        draw_simulated_graph(ax, generate_conf(K_rol=3 * i + 1, k_rol=1))
+        for i, ax in enumerate(axes.flat):
+            draw_simulated_graph(ax, 20, 1000,
+                                 mk_conf(K_rol=3 * i + 1, k_rol=1))
 
-    plt.tight_layout()
-    plt.savefig('output_2.png')
+        plt.tight_layout()
+        plt.savefig('output_2.png')
 
-    fig, axes = plt.subplots(nrows=2, ncols=2)
-    fig.set_size_inches(12, 10)
+    tqdm.write('Run the simulation about random learning [y/N]: ', end='')
+    ans = input().lower()
+    if ans == 'y' or ans == 'yes':
+        fig, axes = plt.subplots(nrows=2, ncols=2)
+        fig.set_size_inches(12, 10)
 
-    for i, ax in enumerate(axes.flat):
-        draw_simulated_graph(ax, generate_conf(K_rnd=3 * i + 1, k_rnd=1))
+        for i, ax in enumerate(axes.flat):
+            draw_simulated_graph(ax, 20, 1000,
+                                 mk_conf(K_rnd=3 * i + 1, k_rnd=1))
 
-    plt.tight_layout()
-    plt.savefig('output_3.png')
+        plt.tight_layout()
+        plt.savefig('output_3.png')
