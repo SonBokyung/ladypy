cbff0fee2c02bff5145ff364f0a6a4d6623a680a
PluVian <urisa12@gmail.com>
2017-04-13 11:40:12 +0900

    Added random learning with rho.

--- tools/test_elg_models.py
+++ tools/test_elg_models.py
@@ -25,7 +25,7 @@ def mk_conf(**kargs):
     }
 
 
-def draw_simulated_graph(ax, rep, gen, conf):
+def draw_simul(ax, rep, gen, conf):
     mdl = ELG(**conf)
 
     for _ in trange(rep, desc='Trial'):
@@ -51,56 +51,70 @@ def draw_simulated_graph(ax, rep, gen, conf):
     ax.axis([0, gen, 0, min(conf['obj'], conf['sig']) + 1])
 
 
-if __name__ == '__main__':
-    tqdm.write('Run the simulation about parental learning [y/N]: ', end='')
-    ans = input().lower()
-    if ans == 'y' or ans == 'yes':
-        fig, axes = plt.subplots(nrows=2, ncols=2)
-        fig.set_size_inches(12, 10)
+tqdm.write('Run the simulation about parental learning [y/N]: ', end='')
+ans = input().lower()
+if ans == 'y' or ans == 'yes':
+    fig, axes = plt.subplots(nrows=2, ncols=2)
+    fig.set_size_inches(12, 10)
 
-        for i, ax in enumerate(axes.flat):
-            draw_simulated_graph(ax, 1, 100, mk_conf(k_par=3 * i + 1))
+    for i, ax in enumerate(axes.flat):
+        draw_simul(ax, 1, 100, mk_conf(k_par=3 * i + 1))
 
-        plt.tight_layout(True)
-        plt.savefig('output_1.png')
+    plt.tight_layout(True)
+    plt.savefig('output_1.png')
 
-    tqdm.write('Run the simulation about role model learning [y/N]: ', end='')
-    ans = input().lower()
-    if ans == 'y' or ans == 'yes':
-        fig, axes = plt.subplots(nrows=2, ncols=2)
-        fig.set_size_inches(12, 10)
+tqdm.write('Run the simulation about role model learning [y/N]: ', end='')
+ans = input().lower()
+if ans == 'y' or ans == 'yes':
+    fig, axes = plt.subplots(nrows=2, ncols=2)
+    fig.set_size_inches(12, 10)
 
-        for i, ax in enumerate(axes.flat):
-            draw_simulated_graph(ax, 20, 1000,
-                                 mk_conf(K_rol=3 * i + 1, k_rol=1))
+    for i, ax in enumerate(axes.flat):
+        draw_simul(ax, 20, 1000,
+                   mk_conf(K_rol=3 * i + 1, k_rol=1))
 
-        plt.tight_layout(True)
-        plt.savefig('output_2.png')
+    plt.tight_layout(True)
+    plt.savefig('output_2.png')
 
-    tqdm.write('Run the simulation about random learning [y/N]: ', end='')
-    ans = input().lower()
-    if ans == 'y' or ans == 'yes':
-        fig, axes = plt.subplots(nrows=2, ncols=2)
-        fig.set_size_inches(12, 10)
+tqdm.write('Run the simulation about random learning [y/N]: ', end='')
+ans = input().lower()
+if ans == 'y' or ans == 'yes':
+    fig, axes = plt.subplots(nrows=2, ncols=2)
+    fig.set_size_inches(12, 10)
 
-        for i, ax in enumerate(axes.flat):
-            draw_simulated_graph(ax, 20, 5000,
-                                 mk_conf(K_rnd=3 * i + 1, k_rnd=1))
+    for i, ax in enumerate(axes.flat):
+        draw_simul(ax, 20, 5000,
+                   mk_conf(K_rnd=3 * i + 1, k_rnd=1))
 
-        plt.tight_layout(True)
-        plt.savefig('output_3.png')
+    plt.tight_layout(True)
+    plt.savefig('output_3.png')
 
-    tqdm.write(
-        'Run the simulation about parental learning with rho [y/N]: ',
-        end='')
-    ans = input().lower()
-    if ans == 'y' or ans == 'yes':
-        fig, axes = plt.subplots(nrows=2, ncols=2)
-        fig.set_size_inches(12, 10)
+tqdm.write(
+    'Run the simulation about parental learning with rho [y/N]: ',
+    end='')
+ans = input().lower()
+if ans == 'y' or ans == 'yes':
+    fig, axes = plt.subplots(nrows=2, ncols=2)
+    fig.set_size_inches(12, 10)
 
-        for i, ax in enumerate(axes.flat):
-            draw_simulated_graph(ax, 20, 600,
-                                 mk_conf(k_par=1, rho=(1e-4 * 10 ** i)))
+    for i, ax in enumerate(axes.flat):
+        draw_simul(ax, 20, 600,
+                   mk_conf(k_par=1, rho=(1e-4 * 10 ** i)))
 
-        plt.tight_layout(True)
-        plt.savefig('output_4.png')
+    plt.tight_layout(True)
+    plt.savefig('output_4.png')
+
+tqdm.write(
+    'Run the simulation about random learning with rho [y/N]: ',
+    end='')
+ans = input().lower()
+if ans == 'y' or ans == 'yes':
+    fig, axes = plt.subplots(nrows=2, ncols=2)
+    fig.set_size_inches(12, 10)
+
+    for i, ax in enumerate(axes.flat):
+        draw_simul(ax, 20, 600,
+                   mk_conf(K_rnd=1, k_rnd=1, rho=(1e-5 * 10 ** i)))
+
+    plt.tight_layout(True)
+    plt.savefig('output_5.png')
